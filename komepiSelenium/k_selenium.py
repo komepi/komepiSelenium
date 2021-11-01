b2c7fbc4c7a5f59016abc196052250c4a39e3608
komepi <58720999+komepi@users.noreply.github.com>
2021-11-01 15:01:11 +0900

    ドロップダウンからの選択手法を追加

--- komepiSelenium/k_selenium.py
+++ komepiSelenium/k_selenium.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
+from selenium.webdriver.support.select import Select
 
 d=""
 XPATH = 0
@@ -10,7 +11,10 @@ NAME = 3
 MSG_NOT_FOUD_ELEMENT = "要素[%s]は存在しません."
 MSG_NOT_EXIST_METHOD = "正しい選択手法を選択してください"
 METHOD_LIST = [XPATH, ID, CLASS, NAME]
-
+INDEX = 0
+VALUE = 1
+TEXT = 2
+SELECT_LIST = [INDEX, VALUE, TEXT]
 
 def connect_chrome(path, url, is_watch = True):
     """クロームに接続する
@@ -75,7 +79,29 @@ def click_with_update(path, method = 0, is_check = False, span_time = 1.0, memo 
         print(MSG_NOT_FOUD_ELEMENT % path)
     if not is_check:
         time.sleep(span_time)
-
+        
+def select_drop(data,path,method=0,select_by=0,memo=None):
+    """ドロップダウンから選択する
+    method -> 0:xpath, 1:id, 2:class, 3:name
+    select_by -> 0:index, 1:value, 2:text
+    Args:
+        data (obj): 選択に用いるデータ
+        path (str): 要素を検索するデータ
+        method (int, optional): 検索手法. Defaults to 0.
+        select_by (int, optional): 選択手法. Defaults to 0.
+        memo (str, optional): 何を選択したかメモ用. Defaults to None.
+    """
+    try:
+        m = _select_method(method)
+    except KeyError:
+        print(MSG_NOT_EXIST_METHOD)
+    try:
+        select_obj = d.find_element(m,path)
+    except:
+        print(MSG_NOT_FOUD_ELEMENT % path)
+    select = Select(select_obj)
+    _select_by(select,select_by,data)
+    
 def _select_method(method):
     """検索手法を探す
 
@@ -100,3 +126,24 @@ def _select_method(method):
         m = By.NAME
 
     return m
+
+def _select_by(select,method,value):
+    """選択する
+
+    Args:
+        select (select): selectオブジェクト
+        method (int): 選択手法
+        value (obj): 選択に用いるデータ
+
+    Raises:
+        KeyError: 定められた選択手法以外の番号
+
+    """
+    if method not in SELECT_LIST:
+        raise KeyError
+    if method == INDEX:
+        return select.select_by_index(value)
+    elif method == VALUE:
+        select.select_by_value(value)
+    elif method == TEXT:
+        select.select_by_text(value)
